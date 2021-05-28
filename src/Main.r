ab4aa2a534ae3d97639ebdc86f7957a70f002f3b
Mrozowski, Kornel <kpmrozowski@gmail.com>
2021-05-28 15:38:16 +0200

    data-exploration

--- src/Main.r
+++ src/Main.r
@@ -1,3 +1,10 @@
-data = read.csv("../Datasets/JobChanges/aug_train.csv", header=T, sep=",")
-df = data.frame(data, row.names = NULL, check.rows = FALSE, check.names = TRUE, stringsAsFactors = default.stringsAsFactors())
-print(df)
+data <- read.csv("../Datasets/JobChanges/aug_train.csv", header=T, sep=",")
+df <- data.frame(data, row.names = NULL, check.rows = FALSE, check.names = TRUE, stringsAsFactors = default.stringsAsFactors())
+print(head(df))
+
+
+## ---- data exploration-----------------------------------------------
+library(DataExplorer)
+plot_missing(data) ## Are there missing values, and what is the missing data profile? None in this case
+plot_bar(data) ## How does the categorical frequency for each discrete variable look like?
+plot_histogram(data) ## What is the distribution of each continuous variable?
