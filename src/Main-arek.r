e63240dcf65a20997223e60fbc31328a488ecff9
Akoszewski <akoszews@gmail.com>
2021-06-10 18:01:09 +0200

    Dziala mierzenie accuracy

--- src/Main-arek.r
+++ src/Main-arek.r
@@ -4,6 +4,7 @@ source("PrepareData.r")
 df = read.csv("../Datasets/JobChanges/aug_train.csv", header=T, sep=",", na.strings=c("","NA"))
 df <- df[1:3000,]
 df <- prepareData(df)
+#df <- df[1:3000,]
 
 n_train  <-  round(0.5*nrow(df))
 df_train  <-  df[c(0:n_train),]
@@ -13,4 +14,11 @@ model <- svm(target ~ ., data = df_train, kernel = "linear")
 pred <- predict(model, df_test)
 pred_rounded <-round(pred)
 print(model)
-print(pred_rounded)
+#print(pred_rounded)
+
+results <- data.frame(pred_rounded, df_test$target)
+colnames(results) <- c('Predicted', 'Actual')
+accuracy <- GetAccuracy(results)
+print(accuracy)
+print(paste("Accuracy:", gsub(" ", "", paste(accuracy * 100, "%"))))
+
