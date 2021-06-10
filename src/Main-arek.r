90836f0ed93dfc285d3ec2e8ec4682a2492248b3
Akoszewski <akoszews@gmail.com>
2021-06-10 20:12:52 +0200

    Dziala w Main-arek po polaczeniu nowym kodem z main

--- src/Main-arek.r
+++ src/Main-arek.r
@@ -1,24 +1,43 @@
 source("utils.r")
 source("PrepareData.r")
 
-df = read.csv("../Datasets/JobChanges/aug_train.csv", header=T, sep=",", na.strings=c("","NA"))
-df <- df[1:3000,]
-df <- prepareData(df)
+raw_data = read.csv("../Datasets/JobChanges/aug_train.csv", header=T, sep=",", na.strings=c("","NA"))
+#df <- df[1:3000,]
+data <- prepareData(raw_data)
 #df <- df[1:3000,]
 
-n_train  <-  round(0.5*nrow(df))
-df_train  <-  df[c(0:n_train),]
-df_test   <- df[-c(0:n_train),]
+## --------------------------------------------------------------------------------
+## --Wybranie k próbek z puli do początkowego zbioru trenującego i podpisanie ich--
+## --------------------------------------------------------------------------------
 
-model <- svm(target ~ ., data = df_train, kernel = "linear")
-pred <- predict(model, df_test)
-pred_rounded <-round(pred)
+## set the seed to make your partition reproducible
+set.seed(123)
+
+onePercentOfSamples <- floor(0.1 * nrow(data))
+train_idx <- sample(seq_len(nrow(data)), size = onePercentOfSamples)
+
+training_set <- data[train_idx, ]; # str(training_set)
+validating_set <- data[-train_idx, ]; # str(validating_set)
+correct_answers <- validating_set$target
+validating_set$target <- NULL
+
+## ------------------------------------------------------------------------
+## -----------Trenowanie modelu za pomocą zbioru trenującego---------------
+## ------------------------------------------------------------------------
+
+model <- svm(target ~ ., data = training_set, kernel = "linear")
 print(model)
-#print(pred_rounded)
 
-results <- data.frame(pred_rounded, df_test$target)
+## --------------------------------------------------------------------------------
+## --------------Predykcja na zbiorze walidacyjnym---------------
+## --------------------------------------------------------------------------------
+
+predict(model, newdata = validating_set)
+pred <- predict(model, newdata = validating_set)
+pred_rounded <-round(pred)
+results <- data.frame(pred_rounded, correct_answers)
 colnames(results) <- c('Predicted', 'Actual')
+
 accuracy <- GetAccuracy(results)
 print(accuracy)
 print(paste("Accuracy:", gsub(" ", "", paste(accuracy * 100, "%"))))
-
