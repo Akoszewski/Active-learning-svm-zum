8d5c6f69058a6f95606a02790e5314a370bddad0
Akoszewski <akoszews@gmail.com>
2021-06-12 00:45:44 +0200

    Teraz powinno dzialac

--- src/Main-arek.r
+++ src/Main-arek.r
@@ -8,14 +8,15 @@ raw_data = read.csv("../Datasets/JobChanges/aug_train.csv", header=T, sep=",", n
 #raw_data <- raw_data[1:3000,]
 data <- prepareData(raw_data)
 set.seed(1235)
-data <-data[sample(nrow(data)),] # shuffle data
+# data <-data[sample(nrow(data)),] # shuffle data
 
 k <- 1
 initial_train_size <- 2
-
+iterations <- 20
+random <- FALSE
 # Demonstration of the active learning testing function
-accuracies_random <- ActiveLearningTest(data, initial_train_size, k, 20, TRUE)
-accuracies_conf <- ActiveLearningTest(data, initial_train_size, k, 20, FALSE)
+accuracies_random <- ActiveLearningTest(data, initial_train_size, k, iterations, TRUE)
+accuracies_conf <- ActiveLearningTest(data, initial_train_size, k, iterations, random)
 
 wykres = ggplot() + 
   geom_line(data = accuracies_random, aes(x = LabelledDataSize, y = Accuracy, colour = "accuracies_random")) +
